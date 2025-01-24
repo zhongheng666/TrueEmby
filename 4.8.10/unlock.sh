d22aab4bd324c756a62378cf73067d3b50aec177
zhongheng666 <lyh9420@126.com>
2025-01-24 20:00:14 +0800

    4.8.10

---
+++ 4.8.10/unlock.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://github.com/zhongheng666/TrueEmby/raw/refs/heads/main/4.8.10/Emby.Server.Implementations.dll -O /opt/emby-server/system/Emby.Server.Implementations.dll
+wget https://github.com/zhongheng666/TrueEmby/raw/refs/heads/main/4.8.10/Emby.Web.dll -O /opt/emby-server/system/Emby.Web.dll
+wget https://github.com/zhongheng666/TrueEmby/raw/refs/heads/main/4.8.10/MediaBrowser.Model.dll -O /opt/emby-server/system/MediaBrowser.Model.dll
+wget https://github.com/zhongheng666/TrueEmby/raw/refs/heads/main/4.8.10/embypremiere.js -O /opt/emby-server/system/dashboard-ui/embypremiere/embypremiere.js
