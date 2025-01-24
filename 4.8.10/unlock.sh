5c7af4111c615a84dd7f0e73135265cf41cf556e
zhongheng666 <lyh9420@126.com>
2025-01-24 20:03:27 +0800

    4.8.10

--- 4.8.10/unlock.sh
+++ 4.8.10/unlock.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://github.com/zhongheng666/TrueEmby/raw/refs/heads/main/4.8.10/Emby.Server.Implementations.dll -O /opt/emby-server/system/Emby.Server.Implementations.dll
-wget https://github.com/zhongheng666/TrueEmby/raw/refs/heads/main/4.8.10/Emby.Web.dll -O /opt/emby-server/system/Emby.Web.dll
-wget https://github.com/zhongheng666/TrueEmby/raw/refs/heads/main/4.8.10/MediaBrowser.Model.dll -O /opt/emby-server/system/MediaBrowser.Model.dll
-wget https://github.com/zhongheng666/TrueEmby/raw/refs/heads/main/4.8.10/embypremiere.js -O /opt/emby-server/system/dashboard-ui/embypremiere/embypremiere.js
+wget https://github.com/zhongheng666/TrueEmby/raw/refs/heads/main/4.8.10/Emby.Server.Implementations.dll -O /system/Emby.Server.Implementations.dll
+wget https://github.com/zhongheng666/TrueEmby/raw/refs/heads/main/4.8.10/Emby.Web.dll -O /system/Emby.Web.dll
+wget https://github.com/zhongheng666/TrueEmby/raw/refs/heads/main/4.8.10/MediaBrowser.Model.dll -O /system/MediaBrowser.Model.dll
+wget https://github.com/zhongheng666/TrueEmby/raw/refs/heads/main/4.8.10/embypremiere.js -O /system/dashboard-ui/embypremiere/embypremiere.js
